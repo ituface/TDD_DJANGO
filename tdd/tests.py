db5a3ed34c799af9e0e02a46b3537bf8581c6f0b
ituface <ituface@163.com>
2019-03-30 18:40:45 +0800

    将hmtl放到了templates中，使用render_to_string将html转换为字符串------tests已测试通过

--- tdd/tests.py
+++ tdd/tests.py
@@ -3,6 +3,8 @@ from django.core.urlresolvers import resolve
 from tdd.views import home_page
 from django.http import HttpRequest
 # Create your tests here.
+
+from django.template.loader import  render_to_string
 class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
         found=resolve('/')
@@ -10,6 +12,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request=HttpRequest()
         response=home_page(request)
+        expected_html=render_to_string('home.html')#获取页面内容
+        self.assertEqual(response.content.decode(),expected_html)
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>',response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
