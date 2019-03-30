9eca08cc45c35976657e388c3a6e7706c213e063
ituface <ituface@163.com>
2019-03-30 23:10:29 +0800

    增加了ORM（数据库映射），增加了tests（单元测试）---测试通过

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
         self.brower.implicitly_wait(3)
     def tearDown(self):
         self.brower.quit()
+
+    def check_for_row_in_list_table(self,row_text):
+        table=self.brower.find_element_by_id('id_list_table')
+        rows=table.find_element_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
     def test_can_start_a_list_and_retrieve_it_later(self):
          self.brower.get('http://localhost:8000')
          self.assertIn('To-Do',self.brower.title)
@@ -27,6 +32,18 @@ class NewVisitorTest(unittest.TestCase):
              '1:Buy peacock feathers' ,[row.text for row in rows])
 
          self.assertIn('2:Use peacock feathers to make a fly',[row.text for row in rows])
+
+         #她按了回车键后，页面更新了
+         #待办事项表格中显示了'1：Buy peacock feathers'
+         inputbox.send_keys(Keys.ENTER)
+         self.check_for_row_in_list_table('1:Buy peacock feathers')
+
+         inputbox=self.brower.find_element_by_id('id_new_item')
+         inputbox.send_keys('Use peapcock feathers to make a fly')
+         inputbox.send_keys(Keys.ENTER)
+
+         self.check_for_row_in_list_table('1:Buy peacock feathers')
+         self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
          self.fail('Finsh the test')
 
 if __name__=='__main__':
